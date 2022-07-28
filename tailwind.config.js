6aae26e789576258e482203a03dff5ab40dae40e
yash-khaitan <85894670+yash-khaitan@users.noreply.github.com>
2022-07-28 21:22:36 +0530

    Debugged background image.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        rectangle: "url('./Rectangle.png') ",
+      },
+    },
   },
   plugins: [],
 };
